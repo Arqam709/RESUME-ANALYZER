5cd7fe66324a54d4f06f01f5037ea6a9579e2f70
Arqam709 <arqamwaqar42@gmail.com>
2026-05-16 17:53:05 +0300

    Add new Vercel URL to CORS

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -132,7 +132,13 @@ ${jobDescription}
   const result = await model.generateContent(prompt);
 
 const response = await result.response;
-const text = response.text();
+
+let text = response.text();
+
+text = text
+  .replace(/```json/g, "")
+  .replace(/```/g, "")
+  .trim();
 
 return JSON.parse(text);
 
